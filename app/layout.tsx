9cf6e1c1f35a30282225c08feeb7be0ca3f02d21
Yuliya Buturlia <buturlia.y@northeastern.edu>
2025-05-21 11:29:52 +0100

    add vercel page analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Poppins } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
 
 const poppins = Poppins({
   weight: '300',
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={poppins.variable}>
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
